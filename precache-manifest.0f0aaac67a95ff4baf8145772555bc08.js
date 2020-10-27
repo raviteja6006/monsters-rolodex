023ff6117d95e7e2ca5f7c77525673a5b01bde42
Raviteja B <raviteja6006@users.noreply.github.com>
2020-10-27 14:10:41 +0530

    Updates

---
+++ precache-manifest.0f0aaac67a95ff4baf8145772555bc08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce9e0a4802f62f5b1003bd1f18cd9c94",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "42bc305ab1bf5069ec6e",
+    "url": "/monsters-rolodex/static/css/main.4b829449.chunk.css"
+  },
+  {
+    "revision": "17e7a3acfa6de3ea2ab4",
+    "url": "/monsters-rolodex/static/js/2.faf01414.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.faf01414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42bc305ab1bf5069ec6e",
+    "url": "/monsters-rolodex/static/js/main.f8820230.chunk.js"
+  },
+  {
+    "revision": "893285d6c3aa078a52a1",
+    "url": "/monsters-rolodex/static/js/runtime-main.9af6a435.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
